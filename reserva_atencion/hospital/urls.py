998cc429272c97b9bcda30a17a3bd20f34f835de
squirogar <quirogaseb59@gmail.com>
2024-03-24 21:17:12 -0300

    avance en interfaz, plantilla reservar_atencion

--- reserva_atencion/hospital/urls.py
+++ reserva_atencion/hospital/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("historial/", views.historial, name="historial"),
     path("reservar/", views.reservar_atencion, name="reservar_atencion"),
     path("medicos/", views.medicos, name="medicos"),
+    path('get_horas_disponibles/', views.get_horas_disponibles, name='get_horas_disponibles'),
+    path('get_medicos_disponibles/', views.get_medicos_disponibles, name='get_medicos_disponibles'),
+    path('reserva_atencion_medica/', views.reserva_atencion_medica, name='reserva_atencion_medica'),
 ]
--- reserva_atencion/hospital/views.py
+++ reserva_atencion/hospital/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Medico, Atencion
+
 
 from . import utils
 
@@ -28,6 +29,7 @@ def reservar_atencion(request):
 
     print(fechas_semana)
 
+    """
     horarios= [
             "8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30",
             "12:00", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30"
@@ -39,6 +41,7 @@ def reservar_atencion(request):
 
     for i in range(len(fechas_semana["semana"])):
         print("\t\tDIA:", fechas_semana["semana"][i][0])
+        print(type(fechas_semana["semana"][i][1]))
         # atenciones para un dia especifico de la semana
         atenciones_dia = Atencion.objects.filter(fecha_atencion=fechas_semana["semana"][i][1])
         print(atenciones_dia, "\n")
@@ -69,11 +72,10 @@ def reservar_atencion(request):
             lista_horas=lista_horas)
         )
 
-
+    """
     contexto = {
         "hoy": fechas_semana["hoy"],
-        "semana": semana,
-        "horarios": horarios
+        "semana": fechas_semana["semana"],
     }
 
 
@@ -84,6 +86,73 @@ def reservar_atencion(request):
 
     return render(request, "reservar_atencion.html", context=contexto)
 
+
+
+################
+from django.http import JsonResponse
+
+def get_horas_disponibles(request):
+    print("\n\n\nHORAS DISPONIBLES AJAX")
+    fecha = request.GET.get('fecha')
+    print(fecha)
+    atenciones_del_dia = Atencion.objects.filter(fecha_atencion=fecha)
+    print(atenciones_del_dia, "\n")
+
+
+    ###
+    # vamos pregunta por cada horario los medicos disponibles
+    opciones = []
+    horarios= [
+            "8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30",
+            "12:00", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30"
+        ]
+    
+    
+    for hora in horarios:
+        print("\t hora", hora)
+        # Utilizamos exclude() en el modelo Medico para excluir aquellos médicos 
+        # que tienen una atención programada a una determinada hora.
+        #medicos_disponibles = Medico.objects.exclude(atencion__hora_atencion=hora, atencion__fecha_atencion=fechas_semana["semana"][i][1])
+        atencion_con_hora_fecha = Atencion.objects.filter(hora_atencion=hora, fecha_atencion=fecha)
+        medicos_con_atencion = atencion_con_hora_fecha.values_list('medico_id', flat=True)
+        medicos_disponibles = Medico.objects.exclude(id__in=medicos_con_atencion)
+        print("medicos disponibles: ", medicos_disponibles)
+        print(list(medicos_disponibles.values_list("id", "nombre", "apellido")))
+        #atenciones_hora = atenciones_dia.filter(hora_atencion=hora)
+        #print("atenciones_hora", atenciones_hora)
+
+        if medicos_disponibles:
+            # guardamos la lista de medicos en algun lugar junto con la hora
+            opciones.append({"hora": hora, "medicos": list(medicos_disponibles.values_list("id", "nombre", "apellido"))})
+            # opciones = [{hora: lista de medicos disponibles}, ..., {hora: lista de medicos disponibles}]
+
+    return JsonResponse({'opciones': opciones})
+
+
+    ###
+
+
+
+
+def get_medicos_disponibles(request):
+    print("jfdj")
+    selected_value = request.GET.get('selected_value')
+
+    # Aquí realiza la lógica para consultar la base de datos y obtener las opciones para el segundo dropdown
+    # Supongamos que tienes una lista de opciones en forma de tuplas (value, label)
+    options = [
+        {'value': 1, 'label': 'Opción A'},
+        {'value': 2, 'label': 'Opción B'},
+        {'value': 3, 'label': 'Opción C'}
+    ]
+
+    return JsonResponse({'options': options})
+
+################
+
+def reserva_atencion_medica(request):
+    print("aloha")
+    return HttpResponse("aloha")
 
 
 
@@ -149,10 +218,11 @@ def get_semana(year, month, day):
 
     # guardamos cada dia con su correspondiente fecha (nombre_dia, fecha)
     info_semana = {}
-    semana = []
-    for d in zip(nombres_dias, fechas_semana):
-        semana.append(d)
+    semana = {}
+    for d, f in zip(nombres_dias, fechas_semana):
+        semana[d] = f
 
+    print(semana)
     info_semana["hoy"] = date(year, month, day)
     info_semana["semana"] = semana
 
