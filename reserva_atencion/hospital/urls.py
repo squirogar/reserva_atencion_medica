81fba9e22881b37ca94f20567ad479f93238ac94
squirogar <quirogaseb59@gmail.com>
2024-03-25 02:19:39 -0300

    ingresar_Atencion view

--- reserva_atencion/hospital/urls.py
+++ reserva_atencion/hospital/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path("reservar/", views.reservar_atencion, name="reservar_atencion"),
     path("medicos/", views.medicos, name="medicos"),
     path('get_horas_disponibles/', views.get_horas_disponibles, name='get_horas_disponibles'),
-    path('get_medicos_disponibles/', views.get_medicos_disponibles, name='get_medicos_disponibles'),
-    path('reserva_atencion_medica/', views.reserva_atencion_medica, name='reserva_atencion_medica'),
+    path('resultado/', views.ingresar_atencion_medica, name='ingresar_atencion_medica'),
 ]
--- reserva_atencion/hospital/views.py
+++ reserva_atencion/hospital/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, HttpResponse
-from .models import Medico, Atencion
-
+from django.shortcuts import render, redirect
+from .models import Medico, Atencion, Box
+from usuarios.models import Usuario
 
 from . import utils
 
 from datetime import date
 import calendar
 import locale
+
+from django.db import IntegrityError
+
 
 # Create your views here.
 
@@ -29,50 +32,7 @@ def reservar_atencion(request):
 
     print(fechas_semana)
 
-    """
-    horarios= [
-            "8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30",
-            "12:00", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30"
-        ]
-    
-    print("\naca estamos comprobando las atenciones para un dia\n")
 
-    semana = []    
-
-    for i in range(len(fechas_semana["semana"])):
-        print("\t\tDIA:", fechas_semana["semana"][i][0])
-        print(type(fechas_semana["semana"][i][1]))
-        # atenciones para un dia especifico de la semana
-        atenciones_dia = Atencion.objects.filter(fecha_atencion=fechas_semana["semana"][i][1])
-        print(atenciones_dia, "\n")
-
-        # vamos pregunta por cada horario los medicos disponibles
-        lista_horas = []
-        for hora in horarios:
-            print("\t hora", hora)
-            # Utilizamos exclude() en el modelo Medico para excluir aquellos médicos 
-            # que tienen una atención programada a una determinada hora.
-            #medicos_disponibles = Medico.objects.exclude(atencion__hora_atencion=hora, atencion__fecha_atencion=fechas_semana["semana"][i][1])
-            atencion_con_hora_fecha = Atencion.objects.filter(hora_atencion=hora, fecha_atencion=fechas_semana["semana"][i][1])
-            medicos_con_atencion = atencion_con_hora_fecha.values_list('medico_id', flat=True)
-            medicos_disponibles = Medico.objects.exclude(id__in=medicos_con_atencion)
-            print("medicos disponibles: ", medicos_disponibles)
-            #atenciones_hora = atenciones_dia.filter(hora_atencion=hora)
-            #print("atenciones_hora", atenciones_hora)
-
-            if medicos_disponibles:
-                # guardamos la lista de medicos en algun lugar junto con la hora
-                lista_horas.append(utils.Hora(hora, medicos_disponibles))
-
-        
-        # guardamos los dias con su fecha y con sus horas disponibles
-        semana.append(utils.DiaAtencion(
-            dia=fechas_semana["semana"][i][0], 
-            fecha=fechas_semana["semana"][i][1],
-            lista_horas=lista_horas)
-        )
-
-    """
     contexto = {
         "hoy": fechas_semana["hoy"],
         "semana": fechas_semana["semana"],
@@ -132,27 +92,41 @@ def get_horas_disponibles(request):
     ###
 
 
-
-
-def get_medicos_disponibles(request):
-    print("jfdj")
-    selected_value = request.GET.get('selected_value')
-
-    # Aquí realiza la lógica para consultar la base de datos y obtener las opciones para el segundo dropdown
-    # Supongamos que tienes una lista de opciones en forma de tuplas (value, label)
-    options = [
-        {'value': 1, 'label': 'Opción A'},
-        {'value': 2, 'label': 'Opción B'},
-        {'value': 3, 'label': 'Opción C'}
-    ]
-
-    return JsonResponse({'options': options})
-
 ################
 
-def reserva_atencion_medica(request):
+def ingresar_atencion_medica(request):
     print("aloha")
-    return HttpResponse("aloha")
+    if request.method == "POST":
+        fecha = request.POST["fecha"]
+        hora = request.POST["hora"]
+        usuario = request.user.username
+        print("username", usuario)
+        usuario = Usuario.objects.get(username=usuario).id
+        medico = request.POST["medico"]
+        reserva = date.today()
+        print("datos antes de la insercion")
+        print(fecha, hora, usuario, medico, reserva)
+        try:
+            id = Atencion.objects.create(
+                fecha_atencion=fecha, hora_atencion=hora, fecha_reserva=reserva, medico_id=medico, usuario_id=usuario)
+            
+        except IntegrityError as err:
+            print(err)
+            contexto = {"error": True}
+        else:
+            box = Box.objects.get(medico_id = medico)
+            print(box)
+            contexto = {
+                "id": id,
+                "fecha": fecha,
+                "hora": hora,
+                "medico": medico,
+                "box": box,
+                "error": False
+                }
+
+        return render(request, "respuesta_reserva_atencion.html", context=contexto)
+    return redirect("reservar_atencion")
 
 
 
