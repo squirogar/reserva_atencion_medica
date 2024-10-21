9b2a13205391269c02efc736b1974901e9a4595c
Sebastián Quiroga R. <50588970+squirogar@users.noreply.github.com>
2024-10-21 02:32:51 -0300

    Update settings.py
    eliminacion datos de db

--- reserva_atencion/reserva_atencion/settings.py
+++ reserva_atencion/reserva_atencion/settings.py
@@ -92,25 +92,7 @@ WSGI_APPLICATION = 'reserva_atencion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': "reserva_atencion",
-#         "USER": "postgres",
-#         "PASSWORD": "root",
-#         "HOST": "127.0.0.1",
-#         "DATABASE_PORT": "5432",
-#     },
 
-#     'feriados_db': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': "feriados",
-#         "USER": "postgres",
-#         "PASSWORD": "root",
-#         "HOST": "127.0.0.1",
-#         "DATABASE_PORT": "5432",
-#     },
-# }
 DATABASES = {}
 DATABASES["default"] = dj_database_url.config("DJANGO_URL_DB_DEFAULT")
 DATABASES["feriados_db"] = dj_database_url.config("DJANGO_URL_DB_FERIADOS")
